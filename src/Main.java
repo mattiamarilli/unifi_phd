68350db8d9a6a1cd1608874968887029746c942f
mattiamarilli <mattiamarilli@gmail.com>
2022-12-13 08:23:50 +0100

    clean main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 
 import organizationchart.data_access.*;
 public class Main {
-    public static void main(String[] args) {
-        StudentDaoImpl studentDao = new StudentDaoImpl();
-        studentDao.getAllStudent();
-
-    }
+    public static void main(String[] args) {}
 }
