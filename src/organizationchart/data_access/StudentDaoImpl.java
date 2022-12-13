2da05d977ccb84c2012e070322aa819a1e39eeb8
mattiamarilli <mattiamarilli@gmail.com>
2022-12-13 19:06:50 +0100

    some test on the data

--- src/organizationchart/data_access/StudentDaoImpl.java
+++ src/organizationchart/data_access/StudentDaoImpl.java
@@ -6,11 +6,12 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentDaoImpl implements StudentDao {
 
-    static final String DB_URL = "jdbc:mysql://localhost:3306/unifi_phd";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/oc_schema";
     static final String USER = "root";
     static final String PASS = "test";
 
@@ -19,21 +20,25 @@ public class StudentDaoImpl implements StudentDao {
 
     @Override
     public List<Student> getAllStudent() {
+        List<Student> students = new ArrayList<Student>();
         try{
             Connection connection = DriverManager.getConnection(DB_URL,USER,PASS);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM students");
 
             while (resultSet.next()) {
-                // Retrieve by column name
-                System.out.print("name:" + resultSet.getString("name"));
-
+                Student student = new Student(resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getString("surname"),
+                        resultSet.getString("topics")
+                        );
+                students.add(student);
             }
 
         }catch (Exception e){ e.printStackTrace();}
 
 
-        return null;
+        return students;
     }
 
     @Override
