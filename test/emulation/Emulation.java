a1222025f3e6b28d4a1c6614dcf00c7674d83004
mattiamarilli <mattiamarilli@gmail.com>
2023-02-27 09:02:31 +0100

    new test class for usage emulation

---
+++ test/emulation/Emulation.java
@@ -0,0 +1,15 @@
+package emulation;
+
+import org.junit.jupiter.api.Test;
+import organizationchart.server.*;
+import thesisapprovation.server.*;
+import progressreport.server.*;
+import didacticoffer.server.*;
+
+//test class made for emulating usage scenarios of the application
+public class Emulation {
+
+    @Test
+    void name() {
+    }
+}
